b00ae571b7d674a33bf8b8cfcf18bfb391470337
Blase Pinkert <blase.pinkert@gmail.com>
2026-07-20 17:15:20 -0400

    reorganizing

--- test_recipe_handler.py
+++ test_recipe_handler.py
@@ -3,7 +3,7 @@ import pytest
 
 @pytest.fixture
 def handler():
-    return RecipeHandler("recipesTest.csv")
+    return RecipeHandler(".data/recipesTest.csv")
 
 def test_loads_all_recipes(handler):
     recipes = handler.get_all_recipes()
