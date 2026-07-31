5c33c27989677eddd8b34da195d1b29179d47d74
Blase Pinkert <blase.pinkert@gmail.com>
2026-07-31 11:13:03 -0400

    wired main.py to utilize the csv->sql migration

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from app import create_app
 from recipe_handler import RecipeHandler
 from pathlib import Path
 
-DATA_FILE = Path(__file__).parent / "data" / "recipes.csv"
-handler = RecipeHandler(str(DATA_FILE))
+#DATA_FILE = Path(__file__).parent / "data" / "recipes.csv"
+handler = RecipeHandler("data/recipes.db")
 
 
 if __name__ == '__main__':
-    DATA_FILE
+    #DATA_FILE
     handler
     app = create_app(handler)
     app.run(debug=True)
